4f9812ea1dd424b76a0f7c852599e71fb4822500
Shadowfyre781 <shadowfyre781@gmail.com>
2026-05-27 13:45:38 -0400

    Delete src/main/java/nox/shadowfyre/glowingalchemy directory

--- src/main/java/nox/shadowfyre/glowingalchemy/GlowColor.java
+++
@@ -1,4 +0,0 @@
-package nox.shadowfyre.glowingalchemy;
-import net.minecraft.world.level.material.MapColor;
-
-public record GlowColor(String name, int colorHex, String subfolder, MapColor mapColor) {}
